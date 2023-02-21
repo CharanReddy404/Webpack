c9bbb9830d5ec6132ae6b41377e4b74c903b27ca
CharanReddy404 <charanlucky6143@gmail.com>
2023-02-21 09:31:49 +0530

    first commit

---
+++ 0 Webpack demo starter/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'my-first-webpack.bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  mode: 'production',
+};
